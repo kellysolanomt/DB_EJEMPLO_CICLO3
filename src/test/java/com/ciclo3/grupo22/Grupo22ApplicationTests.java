905dba8f1634feab72ec6a341492656b64f3de82
Kelly Solano <kelly.solano.mt@correo.usa.edu.co>
2021-09-29 23:33:06 -0500

    Creacion proyecto sprintboot g22

---
+++ src/test/java/com/ciclo3/grupo22/Grupo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.grupo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
